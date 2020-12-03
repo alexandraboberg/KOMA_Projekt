f6530e2cc972d3b20c9bc304d61bfed47044ce90
alexandraboberg <alexandra.boberg@outlook.com>
2020-12-03 13:47:42 +0100

    tog bort blankspace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, Component } from 'react'
-import {BrowserRouter as Router, Switch, Route, Link, useParams} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom'
 import './index.css'
 import recipes from './API/API_drinks'
 import Pop from './Sidor/Pop'
@@ -10,66 +10,64 @@ import Help from './Sidor/Help'
 import Recipe from './Sidor/Recipe'
 import Result from './Sidor/Result'
 
-  function App() {
-    
+function App () {
   return (
-   
 
     <div className='screen'>
-      <div></div>
+      <div />
 
-      {/*lägg in statusbilder*/}
-       <div className='statusBar'></div>
-     
+      {/* lägg in statusbilder */}
+      <div className='statusBar' />
+
       {/* Footer icons */}
       <Router>
-          <Switch>
-            <Route path='/Filter'><Filter></Filter></Route>
-            <Route path='/Pop'><Pop></Pop></Route>
-            <Route path='/'><Home></Home></Route>
-            <Route path='/Save'><Save></Save></Route>
-            <Route path='/Help'><Help></Help></Route>
-            {/*<Route path='/Recipe/:id'></Route>*/}
-          </Switch>
-          <div className='footer'>
-            <ul className='icons'>
-              <li>
-                <Link to='/Filter'>  
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4' />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to='/Pop'> 
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to='/'> 
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to='/Save'> 
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to='/Help'> 
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-                    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
-                  </svg>
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <Switch>
+          <Route path='/Filter'><Filter /></Route>
+          <Route path='/Pop'><Pop /></Route>
+          <Route path='/'><Home /></Route>
+          <Route path='/Save'><Save /></Route>
+          <Route path='/Help'><Help /></Route>
+          {/* <Route path='/Recipe/:id'></Route> */}
+        </Switch>
+        <div className='footer'>
+          <ul className='icons'>
+            <li>
+              <Link to='/Filter'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4' />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link to='/Pop'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z' />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link to='/Save'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link to='/Help'>
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </Router>
     </div>
   )
