1333a2f2cfde507aca5082fb585c7fb180c2b565
Malva Jansson <halva.malva@gmail.com>
2020-12-07 16:19:37 +0100

    Fixat save och help.

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ function App () {
         <Switch>
           <Route path='/Filter'><Filter /></Route>
           <Route path='/Pop'><Pop /></Route>
-          <Route path='/'><Home /></Route>
+          <Route exact path='/'><Home /></Route>
           <Route path='/Save'><Save /></Route>
           <Route path='/Help'><Help /></Route>
+          
           {/* <Route path='/Recipe/:id'></Route> */}
         </Switch>
         <div className='footer'>
@@ -53,14 +54,14 @@ function App () {
             </li>
             <li>
               <NavLink to='/Save' activeClassName='penis'>
-                <svg  className='save' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 22 22' stroke='currentcolor'>
+                <svg className='save' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 22 22' stroke='currentcolor'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
                 </svg>
               </NavLink>
             </li>
             <li>
               <NavLink to='/Help' activeClassName='penis'>
-                <svg  className='help' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 22 22' stroke='currentcolor'>
+                <svg className='help' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 22 22' stroke='currentcolor'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
                 </svg>
               </NavLink>
