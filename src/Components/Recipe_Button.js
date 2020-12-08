51192bb67f79897f8f47d50ab5984060f41c1e8a
Frida <74410767+Friday-ImInLove@users.noreply.github.com>
2020-12-08 11:31:12 +0100

    Pop-sidan

--- src/Components/Recipe_Button.js
+++ src/Components/Recipe_Button.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import './Components/Recipe_Button'
+import './Components/Recipe_Button.css'
+import recipes from '../API/API_drinks'
 
 
-const Button_Link = props => {
-  const data = props.data;
+const Button_Link = data => {
   const link = "/Recipe/" + data.id;
+
+  return (
+    <p>{data.name}</p>
+  )
+
+  const test2 = recipes.recipes
+    console.log(test2)
   
   return (
     <Link to = {link} className = "Recipe_Button">
       <div className = "Outline">
         <img className = "drink_pic" src = {data.picture}/>
+        {test2.map(d => (<Home name={d.drink_name}/>))}
         <h1> {data.drink_name} </h1>
       </div>
     </Link>
@@ -25,4 +33,4 @@ function Button () {
   )
 }
 
-export default Button
+export default Recipe_Button
--- src/Sidor/Pop.js
+++ src/Sidor/Pop.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState} from 'react'
 import '../index.css'
 import {BrowserRouter as Router, Switch, Route, Link, useParams} from 'react-router-dom';
+import recipes from '../API/API_drinks'
+//import { Collapse, Button, CardBody, Card } from 'reactstrap';
+//import Recipe_Button from '../Recipe_Button.js';
 
-const Pop = () => {
+console.log(recipes.recipes[0].id)
+
+
+
+const Pop =  data => {
+  //const data = props.data;
+  
+  const link = '/drinks/' + data.key;
+ 
+
+  return (
+      <Link to = {link} className = "linkToDrink">
+        <span>
+        <img src={data.image} alt={data.name} className = "drinkPic"></img>
+        <p className = "drinkName">{data.name}</p>
+      </span>
+    </Link>
+  )
+}
+
+
+const testPop = recipes.recipes.filter((recipes) => recipes.drink_name == "P2")
+const testPop2 = recipes.recipes.filter((recipes) => recipes.drink_name == "Rosa Pantern")  
+const testPop3 = recipes.recipes.filter((recipes) => recipes.drink_name == "Mojito")
+const testPop4 = recipes.recipes.filter((recipes) => recipes.drink_name == "Moscow Mule")
+const testPop5 = recipes.recipes.filter((recipes) => recipes.drink_name == "Piña Colada")
+const testPop6 = recipes.recipes.filter((recipes) => recipes.drink_name == "Frozen Strawberry Daiquiri")
+const testPop7 = recipes.recipes.filter((recipes) => recipes.drink_name == "Aperol Spritz")
+const testPop8 = recipes.recipes.filter((recipes) => recipes.drink_name == "Irish Coffee")
+const testPop9 = recipes.recipes.filter((recipes) => recipes.drink_name == "Fizzypop")
+const testPop10 = recipes.recipes.filter((recipes) => recipes.drink_name == "Southside")
+
+
+
+function PopApp() {
+
+      //const [recipes, setRecipes] = useState([])
 
   return (
     <div className='screen'>
@@ -11,8 +50,28 @@ const Pop = () => {
           <button className='back-button'>Back</button>
         </Link>
         <h2>POPULÄRA DRINKAR</h2>
+        {testPop.map(d => <Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>)}
+        {testPop2.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop3.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop4.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop5.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop6.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop7.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop8.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop9.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))}
+        {testPop10.map(d => (<Pop name={d.drink_name} key={d.id} data = {d} image = {d.picture}/>))} 
+        
+      </div>
+      <div>
       </div>
     </div>
   )
 }
-export default Pop
+
+function Recipe_Button(){
+  let{id} = useParams()
+  console.log(id)
+
+}
+
+export default PopApp 
