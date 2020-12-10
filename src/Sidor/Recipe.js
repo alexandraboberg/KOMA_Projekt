9775dbc54e009fe902e33f7162eb65135327b15b
Frida <74410767+Friday-ImInLove@users.noreply.github.com>
2020-12-10 15:02:24 +0100

    Stort hjärta på recipe och updaterar sidan på save button

--- src/Sidor/Recipe.js
+++ src/Sidor/Recipe.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState}  from 'react'
 import { Link, useParams } from 'react-router-dom';
 import recipes from './../API/API_drinks'
 import '../index.css'
+import {saveDrink, getDrinks} from '../Saved_Drinks';
 
 function Instructions(data)  {
   return (
@@ -15,11 +16,24 @@ function Ingridients(data)  {
   )
 }
 
+
+
 const Recipe = () => {
   //Get id from url
   let {id} = useParams()
   //get recipe from id
   let foundRecipe = recipes.recipes.find( r => r.id === id)
+
+  //Save funktion
+  const initialState = getDrinks().includes(foundRecipe.id);
+  const [saved, setSaved] = useState(initialState);
+
+  function handleSave(event){
+    event.preventDefault()
+    saveDrink(foundRecipe.id)
+    setSaved(getDrinks().includes(foundRecipe.id));
+    
+  }
 
   return (
     <div className='screen'>
@@ -33,9 +47,18 @@ const Recipe = () => {
           </button>
         </Link>
         <h2>RECEPT</h2>
+        
       </div>
       <div className='content2'>
+
+        {/* SAVE BUTTON */}
+        <svg className='recipeSaveButton' xmlns='http://www.w3.org/2000/svg' fill={saved ? "#FFDF81" : "none"} viewBox='0 0 22 22' onClick={handleSave}>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
+        </svg>
+
+        {/* RECIPE IMAGE */}
         <img className='recipeImg' src={foundRecipe.picture} alt={foundRecipe.drink_name} />
+        
         <div className='recipeBody'>
           <h2>{foundRecipe.drink_name}</h2>
           <p>{foundRecipe.description}</p>
