fbe97b6687922505997166354520c11923175263
Ia Bruno <ia2010@live.se>
2020-12-12 16:12:13 +0100

    Searchknappen fungerar!

--- src/Sidor/Home.js
+++ src/Sidor/Home.js
@@ -41,6 +41,8 @@ function HomeApp () {
   )
   const limRec = filteredRec.slice(0, 5)
 
+  const linkToSearch = '/Search/' + filter;
+
   return (
     <div>
       {/* ska egentligen va poster2 */}
@@ -48,11 +50,13 @@ function HomeApp () {
       {/* Searchbar */}
       <form id='searchbtn'>
         <input className='searchBar' type='text' placeholder='Sök drinkar' onChange={(e) => setFilter(e.target.value)}  />
-        <button id='search'>
-          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
-          </svg>
-        </button>
+        <Link to={linkToSearch}>
+          <button id='search'>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
+            </svg>
+          </button>
+        </Link>
       </form>
       <div />
       <div id='searchRes'>
